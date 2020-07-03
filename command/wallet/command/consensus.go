cd878a16cf8dd0bdcb898354c44a26cbb10f3f2e
bCoder778 <bCoder778@gmail.com>
2020-07-03 14:39:17 +0800

    Modify wallet command

--- command/wallet/command/consensus.go
+++ command/wallet/command/consensus.go
@@ -8,7 +8,6 @@ import (
 	"github.com/Futuremine-chain/futuremine/futuremine/common/kit/message"
 	private2 "github.com/Futuremine-chain/futuremine/futuremine/common/private"
 	"github.com/Futuremine-chain/futuremine/futuremine/rpc"
-	rpctypes "github.com/Futuremine-chain/futuremine/futuremine/rpc/types"
 	"github.com/Futuremine-chain/futuremine/futuremine/types"
 	"github.com/Futuremine-chain/futuremine/service/p2p"
 	"github.com/Futuremine-chain/futuremine/tools/arry"
@@ -21,152 +20,14 @@ import (
 
 func init() {
 	txCmds := []*cobra.Command{
-		GetMessageCmd,
-		SendMessageCmd,
 		SendCandidateCmd,
 		SendCancelCmd,
 		SendVoteCmd,
+		GetCandidatesCmd,
+		CycleSupersCmd,
 	}
 	RootCmd.AddCommand(txCmds...)
-	RootSubCmdGroups["message"] = txCmds
-
-}
-
-var SendMessageCmd = &cobra.Command{
-	Use:     "SendTransction {from} {to} {token} {amount} {fees} {password} {nonce}; Send a transaction;",
-	Aliases: []string{"sendtransction", "ST", "st"},
-	Short:   "SendTransction {from} {to} {token} {amount} {fees} {password} {nonce}; Send a transaction;",
-	Example: `
-	SendTransaction xCHiGPLCzgnrdTqjKABXZteAGVJu3jXLjnQ xCE9boXz2TxSE9srVPDdfszyiXtfT3vduc8 UB 10 0.1
-		OR
-	SendTransaction xCHiGPLCzgnrdTqjKABXZteAGVJu3jXLjnQ xCE9boXz2TxSE9srVPDdfszyiXtfT3vduc8 UB 10 0.1 123456
-		OR
-	SendTransaction xCHiGPLCzgnrdTqjKABXZteAGVJu3jXLjnQ xCE9boXz2TxSE9srVPDdfszyiXtfT3vduc8 UB 10 0.1 123456 1
-	`,
-	Args: cobra.MinimumNArgs(5),
-	Run:  SendTransaction,
-}
-
-func SendTransaction(cmd *cobra.Command, args []string) {
-	var passwd []byte
-	var err error
-	if len(args) > 5 {
-		passwd = []byte(args[5])
-	} else {
-		fmt.Println("please input password：")
-		passwd, err = readPassWd()
-		if err != nil {
-			log.Error(cmd.Use+" err: ", fmt.Errorf("read password failed! %s", err.Error()))
-			return
-		}
-	}
-	privKey, err := loadPrivate(getAddJsonPath(args[0]), passwd)
-	if err != nil {
-		log.Error(cmd.Use+" err: ", fmt.Errorf("wrong password"))
-		return
-	}
-
-	tx, err := parseTransaction(cmd, args)
-	if err != nil {
-		log.Errorf(cmd.Use+" err: %s", err.Error())
-		return
-	}
-	account, err := AccountByRpc(tx.From().String())
-	if err != nil {
-		log.Error(cmd.Use+" err: ", err)
-		return
-	}
-	if tx.Header.Nonce == 0 {
-		tx.Header.Nonce = account.Nonce + 1
-	}
-	if err := signMsg(tx, privKey.Private); err != nil {
-		log.Error(cmd.Use+" err: ", errors.New("signature failure"))
-		return
-	}
-
-	rs, err := sendMsg(tx)
-	if err != nil {
-		log.Error(cmd.Use+" err: ", err)
-	} else if rs.Code != 0 {
-		log.Errorf(cmd.Use+" err: code %d, message: %s", rs.Code, rs.Err)
-	} else {
-		fmt.Println()
-		fmt.Println(string(rs.Result))
-	}
-}
-
-func parseTransaction(cmd *cobra.Command, args []string) (*types.Message, error) {
-	var err error
-	var from, to, token arry.Address
-	var amount, fee, nonce uint64
-	from = arry.StringToAddress(args[0])
-	to = arry.StringToAddress(args[1])
-	token = arry.StringToAddress(args[2])
-	if fAmount, err := strconv.ParseFloat(args[3], 64); err != nil {
-		return nil, errors.New("[amount] wrong")
-	} else {
-		if fAmount < 0 {
-			return nil, errors.New("[amount] wrong")
-		}
-		if amount, err = types.NewAmount(fAmount); err != nil {
-			return nil, errors.New("[amount] wrong")
-		}
-	}
-	if fFees, err := strconv.ParseFloat(args[4], 64); err != nil {
-		return nil, errors.New("[fees] wrong")
-	} else {
-		if fFees < 0 {
-			return nil, errors.New("[fees] wrong")
-		}
-		if fee, err = types.NewAmount(fFees); err != nil {
-			return nil, errors.New("[fees] wrong")
-		}
-	}
-	if len(args) > 6 {
-		nonce, err = strconv.ParseUint(args[6], 10, 64)
-		if err != nil {
-			return nil, errors.New("[nonce] wrong")
-		}
-	}
-	return message.NewTransaction(from, to, token, amount, fee, nonce), nil
-}
-
-func signMsg(msg *types.Message, key string) error {
-	msg.SetHash()
-	priv, err := secp256k1.ParseStringToPrivate(key)
-	if err != nil {
-		return errors.New("[key] wrong")
-	}
-	if err := msg.SignMessage(priv); err != nil {
-		return errors.New("sign failed")
-	}
-	return nil
-}
-
-func sendMsg(msg *types.Message) (*rpc.Response, error) {
-	rpcMsg, err := rpctypes.MsgToRpcMsg(msg)
-	if err != nil {
-		return nil, err
-	}
-	rpcClient, err := NewRpcClient()
-	if err != nil {
-		return nil, err
-	}
-	defer rpcClient.Close()
-
-	jsonBytes, err := json.Marshal(rpcMsg)
-	if err != nil {
-		return nil, err
-	}
-	re := &rpc.Request{Params: jsonBytes}
-	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*20)
-	defer cancel()
-
-	resp, err := rpcClient.Gc.SendMessageRaw(ctx, re)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
+	RootSubCmdGroups["consensus"] = txCmds
 
 }
 
@@ -442,19 +303,27 @@ func parseVote(args []string) (*types.Message, error) {
 	return message.NewVote(from, to, fee, nonce), nil
 }
 
-var GetMessageCmd = &cobra.Command{
-	Use:     "GetMessage {msghash}; Get Message by hash;",
-	Aliases: []string{"getmessage", "GM", "gm"},
-	Short:   "GetMessage {msghash}; Get Message by hash;",
+var GetCandidatesCmd = &cobra.Command{
+	Use:     "GetCandidates",
+	Short:   "GetCandidates;Get current candidates;",
+	Aliases: []string{"getcandidates", "GC", "gc"},
 	Example: `
-	GetMessage 0xef7b92e552dca02c97c9d596d1bf69d0044d95dec4cee0e6a20153e62bce893b
+	GetCandidates
 	`,
-	Args: cobra.MinimumNArgs(1),
-	Run:  GetMessage,
+	Run: GetCandidates,
 }
 
-func GetMessage(cmd *cobra.Command, args []string) {
-	resp, err := GetMessageRpc(args[0])
+func GetCandidates(cmd *cobra.Command, args []string) {
+	client, err := NewRpcClient()
+	if err != nil {
+		log.Error(cmd.Use+" err: ", err)
+		return
+	}
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*20)
+	defer cancel()
+	resp, err := client.Gc.Candidates(ctx, &rpc.Request{})
 	if err != nil {
 		log.Error(cmd.Use+" err: ", err)
 		return
@@ -466,21 +335,46 @@ func GetMessage(cmd *cobra.Command, args []string) {
 	outputRespError(cmd.Use, resp)
 }
 
-func GetMessageRpc(hashStr string) (*rpc.Response, error) {
+var CycleSupersCmd = &cobra.Command{
+	Use:     "CycleSupers {cycle}; Gets the current super nodes;",
+	Short:   "CycleSupers {cycle}; Gets the current super nodes;",
+	Aliases: []string{"cyclesupers", "CS", "cs"},
+	Example: `
+	CycleSupers {8736163}
+	`,
+	Args: cobra.MinimumNArgs(1),
+	Run:  CycleSupers,
+}
+
+func CycleSupers(cmd *cobra.Command, args []string) {
+	term, err := strconv.ParseUint(args[0], 10, 64)
+	if err != nil {
+		log.Error(cmd.Use+" err: ", errors.New("[term] wrong"))
+		return
+	}
 	client, err := NewRpcClient()
 	if err != nil {
-		return nil, err
+		log.Error(cmd.Use+" err: ", err)
+		return
 	}
 	defer client.Close()
-
 	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*20)
 	defer cancel()
 
-	params := []interface{}{hashStr}
+	params := []interface{}{term}
 	if bytes, err := json.Marshal(params); err != nil {
-		return nil, err
+		log.Error(cmd.Use+" err: ", err)
+		return
 	} else {
-		resp, err := client.Gc.GetMessage(ctx, &rpc.Request{Params: bytes})
-		return resp, err
+		resp, err := client.Gc.GetCycleSupers(ctx, &rpc.Request{Params: bytes})
+		if err != nil {
+			log.Error(cmd.Use+" err: ", err)
+			return
+		}
+		if resp.Code == 0 {
+			output(string(resp.Result))
+			return
+		}
+		outputRespError(cmd.Use, resp)
 	}
 }
--- command/wallet/command/node.go
+++ command/wallet/command/node.go
@@ -2,23 +2,17 @@ package command
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"github.com/Futuremine-chain/futuremine/futuremine/rpc"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"strconv"
 	"time"
 )
 
 func init() {
 	nodeCmds := []*cobra.Command{
-		LastHeightCmd,
 		MsgPoolCmd,
-		PeerInfoCmd,
-		GetCandidatesCmd,
-		CycleSupersCmd,
 		LocalInfoCmd,
+		PeerInfoCmd,
 	}
 	RootCmd.AddCommand(nodeCmds...)
 	RootSubCmdGroups["node"] = nodeCmds
@@ -60,82 +54,6 @@ func MsgPool(cmd *cobra.Command, args []string) {
 	outputRespError(cmd.Use, resp)
 }
 
-var GetCandidatesCmd = &cobra.Command{
-	Use:     "GetCandidates",
-	Short:   "GetCandidates;Get current candidates;",
-	Aliases: []string{"getcandidates", "GC", "gc"},
-	Example: `
-	GetCandidates
-	`,
-	Run: GetCandidates,
-}
-
-func GetCandidates(cmd *cobra.Command, args []string) {
-	client, err := NewRpcClient()
-	if err != nil {
-		log.Error(cmd.Use+" err: ", err)
-		return
-	}
-	defer client.Close()
-
-	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*20)
-	defer cancel()
-	resp, err := client.Gc.Candidates(ctx, &rpc.Request{})
-	if err != nil {
-		log.Error(cmd.Use+" err: ", err)
-		return
-	}
-	if resp.Code == 0 {
-		output(string(resp.Result))
-		return
-	}
-	outputRespError(cmd.Use, resp)
-}
-
-var CycleSupersCmd = &cobra.Command{
-	Use:     "CycleSupers {cycle}; Gets the current super nodes;",
-	Short:   "CycleSupers {cycle}; Gets the current super nodes;",
-	Aliases: []string{"cyclesupers", "CS", "cs"},
-	Example: `
-	CycleSupers {8736163}
-	`,
-	Args: cobra.MinimumNArgs(1),
-	Run:  CycleSupers,
-}
-
-func CycleSupers(cmd *cobra.Command, args []string) {
-	term, err := strconv.ParseUint(args[0], 10, 64)
-	if err != nil {
-		log.Error(cmd.Use+" err: ", errors.New("[term] wrong"))
-		return
-	}
-	client, err := NewRpcClient()
-	if err != nil {
-		log.Error(cmd.Use+" err: ", err)
-		return
-	}
-	defer client.Close()
-	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*20)
-	defer cancel()
-
-	params := []interface{}{term}
-	if bytes, err := json.Marshal(params); err != nil {
-		log.Error(cmd.Use+" err: ", err)
-		return
-	} else {
-		resp, err := client.Gc.GetCycleSupers(ctx, &rpc.Request{Params: bytes})
-		if err != nil {
-			log.Error(cmd.Use+" err: ", err)
-			return
-		}
-		if resp.Code == 0 {
-			output(string(resp.Result))
-			return
-		}
-		outputRespError(cmd.Use, resp)
-	}
-}
-
 var PeerInfoCmd = &cobra.Command{
 	Use:     "PeerInfo",
 	Short:   "PeerInfo; Get peer info;",
@@ -158,39 +76,6 @@ func PeerInfo(cmd *cobra.Command, args []string) {
 	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*20)
 	defer cancel()
 	resp, err := client.Gc.PeersInfo(ctx, &rpc.Request{})
-	if err != nil {
-		log.Error(cmd.Use+" err: ", err)
-		return
-	}
-	if resp.Code == 0 {
-		output(string(resp.Result))
-		return
-	}
-	outputRespError(cmd.Use, resp)
-}
-
-var LastHeightCmd = &cobra.Command{
-	Use:     "LastHeight",
-	Short:   "LastHeight; Get last height of node;",
-	Aliases: []string{"lastheight", "LH", "lh"},
-	Example: `
-	LastHeight 
-	`,
-	Args: cobra.MinimumNArgs(0),
-	Run:  LastHeight,
-}
-
-func LastHeight(cmd *cobra.Command, args []string) {
-	client, err := NewRpcClient()
-	if err != nil {
-		log.Error(cmd.Use+" err: ", err)
-		return
-	}
-	defer client.Close()
-
-	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*20)
-	defer cancel()
-	resp, err := client.Gc.LastHeight(ctx, &rpc.Request{})
 	if err != nil {
 		log.Error(cmd.Use+" err: ", err)
 		return
